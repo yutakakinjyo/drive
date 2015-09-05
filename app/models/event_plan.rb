8f32cca06251b4955db10cbe8836b22bbbce0d8f
yutakakinjyo <yutakakinjyo@gmail.com>
2015-09-05 05:32:29 +0000

    remoe id sufix from event plan

--- app/models/event_plan.rb
+++ app/models/event_plan.rb
@@ -3,11 +3,11 @@ class EventPlan < Issue
   has_many :sponsors
 
   def set_task(content)
-    Task.create(content: content, event_plan_id: self.id)
+    Task.create(content: content, event_plan: self)
   end
 
   def set_sponcer(name)
-    Sponsor.create(name: name, event_plan_id: self.id)
+    Sponsor.create(name: name, event_plan: self)
   end
 
 end
