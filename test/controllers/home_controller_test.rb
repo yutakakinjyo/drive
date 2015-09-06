f319d3cb9315f4b39c3a12bfebf5822dea5624f4
yutakakinjyo <yutakakinjyo@gmail.com>
2015-09-06 16:26:58 +0000

    added test cases.
    mtg.yml effect home_controller_test so I have deleted fixture data from yml file.
    But I don't know when apply fixture date to database, why only mtg.yml file.

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -3,13 +3,14 @@ require 'test_helper'
 class HomeControllerTest < ActionController::TestCase
 
   def setup
-   session[:member_id] = Member.create(name: "yutaka").id
+    session[:member_id] = Member.create(name: "user").id
   end
 
   test "should get index" do
     get :index
     assert_response :success
     assert_not_nil assigns(:issues)
+    assert_not_nil assigns(:new_issue)
   end
 
 end
--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @member = Member.create( provider: 'facebook', uid: '12345' )
+    @request.env["omniauth.auth"] = { provider: 'facebook', uid: '12345', info: { name: "user" } }
+  end
+
+  test "should exist member id key in session" do
+    get :create
+    assert_response :redirect
+    assert_equal @member.id, session[:member_id]
+    assert_redirected_to root_path
+  end
+
+  test "should not exist member id in session" do
+    get :destroy
+    assert_response :redirect
+    assert_nil session[:member_id]
+    assert_redirected_to root_path
+  end
+
 end
