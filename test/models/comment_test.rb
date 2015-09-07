769c0774828fc2fb06db5b879a3e30945a585a2f
yutakakinjyo <yutakakinjyo@gmail.com>
2015-09-07 06:32:01 +0000

    add validation

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -3,16 +3,31 @@ require 'test_helper'
 class CommentTest < ActiveSupport::TestCase
 
   def setup
-    @comment = Comment.create
+    @member = Member.create
+    @issue = Issue.create(title: "issue", owner: @member)
+
+    @comment = Comment.create(issue: @issue, member: @member)
     @member1 = Member.create
     @member2 = Member.create
+  end
+
+  test "invalid issue" do
+    assert Comment.create(issue: nil, member: @member).invalid?
+  end
+
+  test "invalid member" do
+    assert Comment.create(issue: @issue, member: nil).invalid?
+  end
+
+  test "valid member" do
+    assert Comment.create(issue: @issue, member: @member).valid?
   end
 
   test "liked" do
     @comment.liked(@member1)
     @comment.liked(@member2)
     
-    refute_nil @comment.likes
+    assert_not_nil @comment.likes
     assert_equal @member1, @comment.likes.first.member
     assert_equal @member2, @comment.likes.second.member
 
