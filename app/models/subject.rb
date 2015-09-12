57b51725520ddb8fd4e4fa2670448bbc5cfacd3d
yutakakinjyo <yutakakinjyo@gmail.com>
2015-09-12 08:27:32 +0000

    add update func

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,7 +2,20 @@ class Subject < ActiveRecord::Base
 
   belongs_to :event
   belongs_to :owner, class_name: 'Member'
+  belongs_to :prev_state_subject, class_name: 'Subject'
 
   enum state: [:UNDECIDED, :READY, :DECIDED]
 
+  def progress_update(params)
+    prev = self.copy_create
+    self.prev_state_subject = prev
+    self.update(params)
+  end
+
+  def copy_create
+    Subject.create(title: self.title, content: self.content, 
+                   state: self.state, prev_state_subject: self.prev_state_subject)
+  end  
+
 end
+
--- test/models/subject_test.rb
+++ test/models/subject_test.rb
@@ -1,7 +1,29 @@
+# -*- coding: utf-8 -*-
 require 'test_helper'
 
 class SubjectTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @mtg = Event.mtg_planed(title: "タイトル")
+    @subject = @mtg.subjects.first
+  end
+
+  test "progress update" do
+    @subject.progress_update({content: "MTG"})
+
+    assert_equal "MTG", @subject.content
+    assert_equal nil, @subject.prev_state_subject.content
+  end
+
+  test "many progress update" do
+
+    @subject.progress_update({content: "MTG"})
+    @subject.progress_update({content: "オフラインMTG"})
+
+    assert_equal "オフラインMTG", @subject.content
+    assert_equal "MTG",           @subject.prev_state_subject.content
+    assert_equal nil,             @subject.prev_state_subject.prev_state_subject.content
+  end
+
+
 end
