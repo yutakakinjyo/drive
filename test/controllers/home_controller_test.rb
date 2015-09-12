f46148924873b0304aa3f7b402b4674728e5da03
yutakakinjyo <yutakakinjyo@gmail.com>
2015-09-12 17:08:24 +0000

    add authenticate tests

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -3,7 +3,17 @@ require 'test_helper'
 class HomeControllerTest < ActionController::TestCase
 
   def setup
+  end
+
+  test "redirect front page to authentication" do
+    get :index
+    assert_response :redirect
+  end
+
+  test "succuess front page to authentication" do
     session[:member_id] = Member.create(name: "user").id
+    get :index
+    assert_response :success
   end
 
 
