5ced84662ca95f31b06f9819a7cbad8e425f37f6
yutakakinjyo <yutakakinjyo@gmail.com>
2015-09-14 15:31:24 +0000

    add mtg crate controller test

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -5,8 +5,8 @@ class EventsControllerTest < ActionController::TestCase
 
   def setup
     session[:member_id] = Member.create
-    community = Community.create(name: "Java Kueche")
-    @event = Event.mtg_planed(title: "タイトル", content: "MTG", community: community)
+    @community = Community.create(name: "Java Kueche")
+    @event = Event.mtg_planed(title: "タイトル", content: "MTG", community: @community)
   end
 
   test "should get show" do
@@ -15,5 +15,16 @@ class EventsControllerTest < ActionController::TestCase
     assert_not_nil @event.subjects.first
     assert_response :success
   end
+
+  test "should redirect to event page" do
+    post :create, mtg: { community_id: @community.id,  subjects_attributes: { "0" => {title: "title", content: "hoge"} } }
+
+    assert_equal 'title', assigns(:mtg).subjects.first.title
+    assert_equal 'hoge',  assigns(:mtg).subjects.first.content
+    assert_redirected_to event_path(assigns(:mtg))
+
+  end
+
+
 
 end
